0f5d2697c5103fa9e8a90892224bd9423c95c2a5
Sheela08 <77281915+Sheela08@users.noreply.github.com>
2022-02-07 11:16:15 +0530

    Update Size.cpp

--- Size.cpp
+++ Size.cpp
@@ -4,12 +4,9 @@ int main() {
     float floatType;
     double doubleType;
     char charType;
-
-    // sizeof evaluates the size of a variable
     printf("Size of int: %zu bytes\n", sizeof(intType));
     printf("Size of float: %zu bytes\n", sizeof(floatType));
     printf("Size of double: %zu bytes\n", sizeof(doubleType));
     printf("Size of char: %zu byte\n", sizeof(charType));
-    
     return 0;
 }
